0ff6d8a8faff30eb67e57702fdd3af948939b28e
amagranercifp <antonio.magraner@cifpceuta.es>
2023-11-07 15:56:41 +0100

    Añadido boton delete item pero sin funcionalidad

--- app/src/main/java/com/example/apprecyclerview/MiRecyclerViewAdapter.java
+++ app/src/main/java/com/example/apprecyclerview/MiRecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import android.nfc.cardemulation.CardEmulation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -26,14 +27,15 @@ public class MiRecyclerViewAdapter extends RecyclerView.Adapter<MiRecyclerViewAd
     }
 
     @Override
-    public int getItemCount(){return cardList.size();}
-
-
-    @Override
     public MiRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
         View view = inflater.inflate(R.layout.list_element,null);
-        return new MiRecyclerViewAdapter.ViewHolder(view);
+        ViewHolder holder = new MiRecyclerViewAdapter.ViewHolder(view);
+        return holder;
     }
+
+    @Override
+    public int getItemCount(){return cardList.size();}
+
 
     @Override
     public void onBindViewHolder(final MiRecyclerViewAdapter.ViewHolder holder, final int position){
@@ -47,12 +49,24 @@ public class MiRecyclerViewAdapter extends RecyclerView.Adapter<MiRecyclerViewAd
         TextView tvCodigoColor, tvNombreColor;
         View viewColor;
 
+        ImageButton imbBorrar;
+
+        private MiRecyclerViewAdapter adapter;
+
         ViewHolder(View itemView){
             super(itemView);
 
             tvCodigoColor = itemView.findViewById(R.id.tvCodigoColor);
             tvNombreColor = itemView.findViewById(R.id.tvNombreColor);
             viewColor = itemView.findViewById(R.id.viewColor);
+            imbBorrar = itemView.findViewById(R.id.imbBorrar);
+
+            imbBorrar.setOnClickListener(view -> {
+                int posicion = getAdapterPosition();
+                adapter.cardList.remove(getAdapterPosition());
+                adapter.notifyItemRemoved(getAdapterPosition());
+            });
+
 
         }
 
